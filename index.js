4408034ab4afc6faf54907d0d7f06cbb7d079467
ddphat2010 <duongphatwish2010@gmail.com>
2024-04-16 00:07:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require ("express");
+
+const app = express ();
+
+const port = 3000;
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Trang chủ</h1>");
+})
+app.get("/products", (req, res)=>{
+    res.send("<h1>Trang danh sách sản phẩm</h1>");
+})
+
+app.listen(port,() =>{
+    console.log("App running on Port 3000");
+})
