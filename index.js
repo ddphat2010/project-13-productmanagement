eb3951c69255ed1a4c8376e1dd584ad1b6cf443c
ddphat2010 <duongphatwish2010@gmail.com>
2024-04-16 00:22:18 +0700

    doi ten

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res)=>{
     res.send("<h1>Trang chủ</h1>");
 })
 app.get("/products", (req, res)=>{
-    res.send("<h1>Trang danh sách sản phẩm</h1>");
+    res.send("<h1>Trang sản phẩm</h1>");
 })
 
 app.listen(port,() =>{
