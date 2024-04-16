fc84925cb4ee5d82dcfc33f0a1a84d90d177ba70
chaotic <mnrealest161@gmail.com>
2024-04-16 21:18:27 +0700

    create homeRouter - client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,8 @@
+const homeRouter = require("./home.route");
+
 module.exports.routesClient = (app) => {
-    app.get("/", (req, res)=>{
-        res.send("<h1>Trang chủ</h1>");
-    })
+    app.get("/", homeRouter);
+    
     app.get("/products", (req, res)=>{
         res.send("<h1>Trang sản phẩm</h1>");
     })
