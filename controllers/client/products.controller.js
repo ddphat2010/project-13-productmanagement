9b6aa78ff575ce90d07c8e6fdb80b43743e068e3
chaotic <mnrealest161@gmail.com>
2024-04-18 20:14:29 +0700

    get products from the database and render to the interface

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,6 +1,23 @@
+const Product = require("../../models/products.model");
+
 // [GET] /products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false,
+        status: "active"
+    })
+
+    for (const item of products) {
+        item.priceNew = item.price * (1 - item.discountPercentage/100);
+        item.priceNew = item.priceNew.toFixed(0);
+    }
+
+    console.log(products);
+
+  
+
     res.render("./client/pages/products/index.pug", {
-        pageTitle: "Trang Danh Sách Sản Phẩm"
+        pageTitle: "Trang Danh Sách Sản Phẩm",
+        products: products
     });
 }
