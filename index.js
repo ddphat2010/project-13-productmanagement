31ba0b5ea455e8a7eabd84c2f84ed9021aa817bd
chaotic <mnrealest161@gmail.com>
2024-04-18 16:18:50 +0700

    connect database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require ("express");
 const routesClient = require("./routes/client/index.route");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb+srv://mnrealest161:qRXdr8uNSZBSrRuh@project-be.1obtg7e.mongodb.net/project-group13-product-management")
+    .then(() => console.log("Connected!!!"));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
