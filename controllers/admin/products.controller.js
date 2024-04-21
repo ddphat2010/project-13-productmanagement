1b7f83381fd6b2c77f6e21c963f5865a9ea92e37
chaotic <mnrealest161@gmail.com>
2024-04-21 22:46:00 +0700

    create file products in folder controllers - admin

---
+++ controllers/admin/products.controller.js
@@ -0,0 +1,4 @@
+// [GET] /admin/products
+module.exports.index = (req, res) => {
+    res.send("Trang quản lí sản phẩm");
+}
--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const controllers = require("../../controllers/admin/products.controller");
 
-router.get("/", (req, res) => {
-    res.send("Trang quản lí sản phẩm");
-})
+router.get("/", controllers.index);
 
 module.exports = router
