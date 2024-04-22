9417ef0bd473e1bb5a28afdb6111ce6815ce5d2c
ddphat2010 <duongphatwish2010@gmail.com>
2024-04-22 22:19:48 +0700

    update file controller

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,4 +1,6 @@
 // [GET] /admin/products
 module.exports.index = (req, res) => {
-    res.render("./admin/pages/products/index.pug");
+    res.render("./admin/pages/products/index.pug",{
+        pageTitle:"Trang Quan Li San Pham"
+    });
 }
