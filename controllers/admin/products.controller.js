80fa3c7f2281c1e70612a07c1c7978e2b56b7592
chaotic <mnrealest161@gmail.com>
2024-04-22 22:38:28 +0700

    up to date

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -2,5 +2,7 @@ const Product = require("../../models/products.model");
 
 // [GET] /admin/products
 module.exports.index = (req, res) => {
-    res.render("./admin/pages/products/index.pug");
+    res.render("./admin/pages/products/index.pug", {
+        pageTitle: "Quản Lí Sản Phẩm"
+    });
 }
