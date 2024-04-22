ada6a17e63f884beab3418415ed6002626084a8a
chaotic <mnrealest161@gmail.com>
2024-04-22 22:27:05 +0700

    update layouts products - admin

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,4 +1,16 @@
+const Product = require("../../models/products.model");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
-    res.render("./admin/pages/products/index.pug");
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    })
+
+    console.log(products);
+
+
+    res.render("./admin/pages/products/index.pug", {
+        pageTitle: "Trang quản lí sản phẩm",
+        products: products
+    });
 }
