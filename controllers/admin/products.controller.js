24b2ed19976ad88326a8db10a31892132c9ed24a
ddphat2010 <duongphatwish2010@gmail.com>
2024-04-23 12:20:41 +0700

    Fix bug

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,8 +1,25 @@
 const Product = require("../../models/products.model");
 
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+// module.exports.index = (req, res) => {
+//     res.render("./admin/pages/products/index.pug", {
+//         pageTitle: "Quản Lí Sản Phẩm"
+//     });
+// }
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false,
+        status: "active"
+    })
+
+    for (const item of products) {
+        item.priceNew = item.price * (1 - item.discountPercentage/100);
+        item.priceNew = item.priceNew.toFixed(0);
+    }
+
+
     res.render("./admin/pages/products/index.pug", {
-        pageTitle: "Quản Lí Sản Phẩm"
+        pageTitle: "Trang Danh Sách Sản Phẩm",
+        products: products
     });
 }
