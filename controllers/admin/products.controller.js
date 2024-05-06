f0c243564ce55e51cd476e345f0a195b29b2b6da
chaotic <mnrealest161@gmail.com>
2024-05-06 20:40:49 +0700

    update product page

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -7,9 +7,18 @@ const Product = require("../../models/products.model");
 //     });
 // }
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
+
+    const find = {
         deleted: false
-    })
+    }
+
+    
+    if(req.query.status) {
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
+
 
     res.render("./admin/pages/products/index.pug", {
         pageTitle: "Trang Danh Sách Sản Phẩm",
