16a58730963eb033d7f998b5c8b97369ed4ff500
chaotic <mnrealest161@gmail.com>
2024-05-06 21:21:02 +0700

    filterStatus feature

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -7,6 +7,33 @@ const Product = require("../../models/products.model");
 //     });
 // }
 module.exports.index = async (req, res) => {
+    const filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+
+    if(req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status) 
+        filterStatus[index].class = "active"
+    } else {
+        filterStatus[0].class = "active"
+    }
+
+    console.log(filterStatus);
+
 
     const find = {
         deleted: false
@@ -22,6 +49,7 @@ module.exports.index = async (req, res) => {
 
     res.render("./admin/pages/products/index.pug", {
         pageTitle: "Trang Danh Sách Sản Phẩm",
-        products: products
+        products: products,
+        filterStatus: filterStatus
     });
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,22 @@
-console.log("OK");
+// buttonStatus
+const buttonStatus = document.querySelectorAll("[button-status]");
+
+if(buttonStatus.length > 0) {
+    const url = new URL(window.location.href);
+
+    buttonStatus.forEach((button) => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+
+            if(status) {
+                url.searchParams.set("status", status);
+            } else {
+                url.searchParams.delete("status");
+            }
+
+            window.location.href = url.href;
+        })
+    })
+}
+
+// end buttonStatus
