dc169c80805a35483b8b4425b9cfc6e125217c2b
chaotic <mnrealest161@gmail.com>
2024-05-10 04:18:19 +0700

    pagination - admin

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -20,19 +20,41 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
+    // Search
     if(req.query.keyword) {
         const keyword = RegExp(req.query.keyword, "i")
         find.title = keyword;
     }
+    // End Search
 
 
-    const products = await Product.find(find);
+    // Pagination
+    const objectPagination = {
+        limitItem : 4,
+        currentPage : 1
+
+    };
+
+    if(req.query.page) {
+        objectPagination.currentPage = parseInt(req.query.page);
+    }
+
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
+
+    const countProducts = await Product.countDocuments(find);
+    objectPagination.totalPage = Math.ceil(countProducts/objectPagination.limitItem)
+
+    console.log(objectPagination);
+    // End Pagination
+
+    const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
 
 
     res.render("./admin/pages/products/index.pug", {
         pageTitle: "Trang Danh Sách Sản Phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: req.query.keyword
+        keyword: req.query.keyword,
+        pagination: objectPagination
     });
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -41,3 +41,25 @@ if(formSearch) {
     })
 }
 // end search
+
+// Pagination
+const buttonPagination = document.querySelectorAll("[button-pagination]");
+
+if(buttonPagination.length > 0) {
+    const url = new URL(window.location.href);
+
+    buttonPagination.forEach((button) => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+            
+            if(page) {
+                url.searchParams.set("page", page);
+            } else {
+                url.searchParams.delete("page");
+            }
+
+            window.location.href = url.href;
+        })
+    })
+}
+// End Pagination
