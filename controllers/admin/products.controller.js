65000c88c83edcf1b24b52584b760eb22650763e
chaotic <mnrealest161@gmail.com>
2024-05-11 15:11:20 +0700

    soft delete 1 product

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -90,12 +90,24 @@ module.exports.changeMulti = async (req, res) => {
 
 // [DELETE] /admin/products/delete/:id
 module.exports.deleteItem = async (req, res) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
 
-    await Product.deleteOne({
-        _id: id
-    })
+        // await Product.deleteOne({
+        //     _id: id
+        // })
+    
+        await Product.updateOne({
+            _id: id
+        }, {
+            deleted: true,
+            deletedAt: new Date()
+        })
 
+    } catch (error) {
+        console.log(error);
+    }
 
-    res.redirect("back");
+    res.redirect("back");  
+
 }
--- models/products.model.js
+++ models/products.model.js
@@ -9,7 +9,8 @@ const productsSchema = new mongoose.Schema({
     thumbnail: String,
     status: String,
     position: Number,
-    deleted: Boolean
+    deleted: Boolean,
+    deletedAt: Date
 })
 
 const Product = mongoose.model("Product", productsSchema, "products");
