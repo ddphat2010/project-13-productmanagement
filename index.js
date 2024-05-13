41b6c60bd257376edc26e6d7d7cce149c85e628d
chaotic <mnrealest161@gmail.com>
2024-05-14 03:27:00 +0700

    TinyMCE basic

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 var flash = require('express-flash');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-
+const path = require("path");
 
 
 dotenv.config()
@@ -36,6 +36,10 @@ app.set("view engine", "pug");
 
 app.use(express.static(`${__dirname}/public`));
 // app.use(express.static("public"))
+
+// New Route to the TinyMCE Node module
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+
 
 // routesClient
 routesClient.routesClient(app);
