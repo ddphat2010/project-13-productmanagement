c3c4989a7e41c32325b38f67119cd796274db30a
chaotic <mnrealest161@gmail.com>
2024-05-13 22:03:02 +0700

    push static files to the Cloud(optimized)

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -3,20 +3,13 @@ const router = express.Router();
 const controllers = require("../../controllers/admin/products.controller");
 const multer  = require('multer');
 const validate = require("../../validates/admin/product.validate");
-const cloudinary = require("cloudinary").v2;
-const streamifier = require("streamifier");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
 
 // const storageMulter = require("../../helpers/storageMulter.helper");
 
 // const upload = multer({ storage: storageMulter() });
 
-// cloudinary
-cloudinary.config({ 
-    cloud_name: "df36bt1xr", 
-    api_key: "899446338953232", 
-    api_secret: "vOaUaehG8wQYCY2n9feikylnpJQ"
-});
-// End cloudinary
 
 const upload = multer();
 
@@ -32,36 +25,7 @@ router.get("/create/", controllers.create);
 
 router.post("/create/", 
 upload.single('thumbnail'), 
-function (req, res, next) {
-    if(req.file) {
-        let streamUpload = (req) => {
-            return new Promise((resolve, reject) => {
-                let stream = cloudinary.uploader.upload_stream(
-                  (error, result) => {
-                    if (result) {
-                      resolve(result);
-                    } else {
-                      reject(error);
-                    }
-                  }
-                );
-    
-              streamifier.createReadStream(req.file.buffer).pipe(stream);
-            });
-        };
-    
-        async function upload(req) {
-            let result = await streamUpload(req);
-            req.body[req.file.fieldname] = result.url
-            next();
-        }
-    
-        upload(req);
-    } else {
-        next();
-    }
-
-},
+uploadCloud.uploadSingle,
 validate.createPost,
 controllers.createPost);
 
@@ -69,36 +33,7 @@ router.get("/edit/:id", controllers.edit);
 
 router.patch("/edit/:id", 
 upload.single('thumbnail'), 
-function (req, res, next) {
-    if(req.file) {
-        let streamUpload = (req) => {
-            return new Promise((resolve, reject) => {
-                let stream = cloudinary.uploader.upload_stream(
-                  (error, result) => {
-                    if (result) {
-                      resolve(result);
-                    } else {
-                      reject(error);
-                    }
-                  }
-                );
-    
-              streamifier.createReadStream(req.file.buffer).pipe(stream);
-            });
-        };
-    
-        async function upload(req) {
-            let result = await streamUpload(req);
-            req.body[req.file.fieldname] = result.url
-            next();
-        }
-    
-        upload(req);
-    } else {
-        next();
-    }
-
-},
+uploadCloud.uploadSingle,
 validate.createPost,
 controllers.editPatch);
 
