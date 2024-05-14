c6a1fabd75b8eedac2a08010ca186f115731b34a
chaotic <mnrealest161@gmail.com>
2024-05-14 23:01:10 +0700

    upload-image

--- routes/admin/index.admin.js
+++ routes/admin/index.admin.js
@@ -1,8 +1,11 @@
 const routesDashboard = require("./dashboard.route");
 const routesProducts = require("./products.route");
+const routesProductsCategory = require("./product-category.route");
 
 module.exports.routesAdmin = (app) => {
     app.use("/admin/dashboard", routesDashboard);
 
     app.use("/admin/products", routesProducts);
+
+    app.use("/admin/products-category", routesProductsCategory);
 }
