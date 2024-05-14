c61b1c4c544efcdc5118baba3328849b265ec0d1
chaotic <mnrealest161@gmail.com>
2024-05-14 20:24:13 +0700

    upload image

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,3 +1,32 @@
 tinymce.init({
-    selector: 'textarea[textarea-mce]'
+    selector: 'textarea[textarea-mce]',
+    plugins: 'image code',
+    image_title: true,
+    automatic_uploads: true,
+    file_picker_types: 'image',
+    file_picker_types: 'image',
+
+    file_picker_callback: (cb, value, meta) => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+
+    input.addEventListener('change', (e) => {
+      const file = e.target.files[0];
+
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        const id = 'blobid' + (new Date()).getTime();
+        const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+        const base64 = reader.result.split(',')[1];
+        const blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        cb(blobInfo.blobUri(), { title: file.name });
+      });
+      reader.readAsDataURL(file);
+    });
+
+    input.click();
+  }
 });
