c3a7955ff24f95f8807921956263126ce07df907
chaotic <mnrealest161@gmail.com>
2024-05-15 22:00:40 +0700

    create roles

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,6 +1,29 @@
+const Role = require("../../models/role.model");
+
 // [GET] /admin/roles
-module.exports.index = (req, res) => {
-    res.render("./admin/pages/roles/index.pug", {
-        pageTitle: "Nhóm Quyền"
+module.exports.index = async (req, res) => {
+    const records = await Role.find({
+        deleted: false
     });
+
+    res.render("./admin/pages/roles/index.pug", {
+        pageTitle: "Nhóm Quyền",
+        records: records
+    });
+}
+
+// [GET]/admin/roles/create
+module.exports.create = (req, res) => {
+    res.render("./admin/pages/roles/create.pug", {
+        pageTitle: "Thêm mới nhóm quyền"
+    });
+}
+
+// [Post]/admin/roles/create
+module.exports.createPost = async (req, res) => {
+    const record = new Role(req.body);
+
+    await record.save()
+    
+    res.redirect("/admin/roles");
 }
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -4,4 +4,8 @@ const controllers = require("../../controllers/admin/role.controller");
 
 router.get("/", controllers.index);
 
+router.get("/create", controllers.create);
+
+router.post("/create", controllers.createPost);
+
 module.exports = router
