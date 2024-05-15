0ad588a7f3a3bfcc724103ceb9aab264c643c842
chaotic <mnrealest161@gmail.com>
2024-05-15 20:30:29 +0700

    update edit  products controller

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -203,12 +203,18 @@ module.exports.edit = async (req, res) => {
             deleted: false
         });
     
-        console.log(product);
+        const records = await ProductCategory.find({
+            deleted: false
+        })
+    
+    
+        const newRecords = createTreeHelper(records);
     
     
         res.render("./admin/pages/products/edit.pug", {
             pageTitle: "Chỉnh sửa sản phẩm",
-            product: product
+            product: product,
+            records: newRecords
         });
     } catch (error) {
         res.redirect("/admin/products");
