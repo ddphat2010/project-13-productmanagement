302c56432de315ca902e584b320eab01ca5bd000
chaotic <mnrealest161@gmail.com>
2024-05-18 22:47:08 +0700

    show list product - client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/products.model");
+const ProductCategory = require("../../models/products-category.model");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -43,4 +44,32 @@ module.exports.detail = async (req, res) => {
     } catch (error) {
         res.redirect("/");
     }
+}
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+    const slugCategory = req.params.slugCategory;
+
+    const category = await ProductCategory.findOne({
+        slug: slugCategory,
+        status: "active",
+        deleted: false
+    });
+
+    const products = await Product.find({    
+        product_category_id: category.id,
+        status: "active",
+        deleted: false
+    }).sort({ position: "desc" })
+
+    for (const item of products) {
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0)
+    }
+
+    console.log(products);
+
+    res.render("./client/pages/products/index.pug", {
+        pageTitle: "Trang Danh Sách Sản Phẩm",
+        products: products
+    });
 }
--- routes/client/products.route.js
+++ routes/client/products.route.js
@@ -4,6 +4,8 @@ const controller = require("../../controllers/client/products.controller");
 
 router.get("/", controller.index);
 
-router.get("/:slug", controller.detail);
+router.get("/:slugCategory", controller.category);
+
+// router.get("/:slug", controller.detail);
 
 module.exports = router;
