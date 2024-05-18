40e8a41e46f978fd5e79c8c84322ea6a7e292b04
chaotic <mnrealest161@gmail.com>
2024-05-18 22:14:19 +0700

    list products latest

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,6 +2,7 @@ const Product = require("../../models/products.model");
 
 // [GET] /
 module.exports.index = async (req, res) => { 
+    // Feature
     const productsFeatured = await Product.find({
         featured: "1",
         status: "active",
@@ -9,13 +10,25 @@ module.exports.index = async (req, res) => {
     }).sort({ position: "desc" }).limit(6);
 
     for (const item of productsFeatured) {
-        item.priceNew = item.price * (100 - item.discountPercentage)/100
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0)
     }
 
-    console.log(productsFeatured);
+    // End Feature
+
+    // Latest
+    const productsNew = await Product.find({
+        status: "active",
+        deleted: false
+    }).sort({ position: "desc" }).limit(6);
+
+    for (const item of productsNew) {
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0)
+    }
+    // End Latest
 
     res.render("./client/pages/home/index.pug", {
         pageTitle: "Trang Chủ",
-        productsFeatured: productsFeatured
+        productsFeatured: productsFeatured,
+        productsNew: productsNew
     });
 }
