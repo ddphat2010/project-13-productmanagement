2e4d026e962a3339923f37d885eb12e7b2aa8f4d
chaotic <mnrealest161@gmail.com>
2024-05-18 19:41:50 +0700

    edit info user

--- controllers/admin/my-account.controller.js
+++ controllers/admin/my-account.controller.js
@@ -1,6 +1,34 @@
+const md5 = require("md5");
+const Account = require("../../models/account.model");
 // [GET] /admin/my-account/
 module.exports.index = async (req, res) => {
     res.render("./admin/pages/my-account/index", {
       pageTitle: "Thông tin cá nhân",
     });
-  };
+};
+
+// [GET] /admin/my-account/edit
+module.exports.edit = async (req, res) => {
+    res.render("./admin/pages/my-account/edit", {
+      pageTitle: "Chỉnh sửa thông tin cá nhân",
+    });
+};
+
+// [PATCH] /admin/my-account/edit
+module.exports.editPatch = async (req, res) => {
+    const id = res.locals.user.id
+
+    if(req.body.password) {
+        req.body.password = md5(req.body.password); 
+    } else {
+        delete req.body.password
+    }
+
+    await Account.updateOne({
+        _id: id
+    },
+        req.body        
+    )
+
+    res.redirect("back");
+};
--- routes/admin/my-account.route.js
+++ routes/admin/my-account.route.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
-
 const controller = require("../../controllers/admin/my-account.controller");
 
+const multer  = require('multer');
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+const upload = multer();
+
+
 router.get("/", controller.index);
+
+router.get("/edit", controller.edit);
+
+router.patch(
+    "/edit",
+    upload.single("avatar"),
+    uploadCloud.uploadSingle,
+    controller.editPatch
+  );
 
 module.exports = router;
