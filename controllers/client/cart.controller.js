342d879da5c2ad1cbcd870706ac49aa5bc85ebb9
chaotic <mnrealest161@gmail.com>
2024-05-19 16:29:29 +0700

    page cart

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,37 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/products.model");
+
+// [GET] /cart/
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+
+  const cart = await Cart.findOne({
+    _id: cartId
+  });
+
+  cart.totalPrice = 0;
+
+  if(cart.products.length > 0) {
+    for (const item of cart.products) {
+      const product = await Product.findOne({
+        _id: item.product_id
+      }).select("thumbnail title slug price discountPercentage");
+
+      product.priceNew = (product.price * (100 - product.discountPercentage)/100).toFixed(0);
+
+      item.productInfo = product;
+
+      item.totalPrice = item.quantity * product.priceNew;
+
+      cart.totalPrice += item.totalPrice;
+    }
+  }
+
+  res.render("client/pages/cart/index", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart
+  });
+};
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const controller = require("../../controllers/client/cart.controller");
 
+router.get("/", controller.index);
+
 router.post("/add/:productId", controller.addPost);
 
 module.exports = router;
