3bb07e7a629fa50b3854911f496bb6c4b39657c5
chaotic <mnrealest161@gmail.com>
2024-05-20 19:14:37 +0700

    update dashboard - admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,4 +1,45 @@
+const Product = require("../../models/products.model");
+
 // [GET] /admin/dashboard
-module.exports.dashboard = (req, res) => {
-    res.render("./admin/pages/dashboard/index.pug");
-}
+module.exports.dashboard = async (req, res) => {
+    const statistic = {
+      categoryProduct: {
+        total: 0,
+        active: 0,
+        inactive: 0,
+      },
+      product: {
+        total: 0,
+        active: 0,
+        inactive: 0,
+      },
+      account: {
+        total: 0,
+        active: 0,
+        inactive: 0,
+      },
+      user: {
+        total: 0,
+        active: 0,
+        inactive: 0,
+      },
+    };
+      // Product
+    statistic.product.total = await Product.countDocuments({
+        deleted: false
+    });
+    statistic.product.active = await Product.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.product.inactive = await Product.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
+  // End Product
+  
+    res.render("admin/pages/dashboard/index", {
+        pageTitle: "Trang tổng quan",
+        statistic: statistic
+    });
+  }
