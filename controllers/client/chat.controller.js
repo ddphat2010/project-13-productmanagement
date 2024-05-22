9e02ee8fd29dc498b2fa2d9f5877b70b45180ec3
chaotic <mnrealest161@gmail.com>
2024-05-22 15:58:08 +0700

    chat optimized

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,52 +1,13 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
-const uploadToCloudinary = require("../../helpers/uploadToCloudinary.helper");
+const chatSocket = require("../../sockets/client/chat.socket");
 
 
 // [GET] /chat/
 module.exports.index = async (req, res) => {
-    const userId = res.locals.user.id;
-    const fullName = res.locals.user.fullName;
 
     // SocketIO
-    _io.once("connection", (socket) => {
-        // Người dùng gửi tin nhắn lên server
-        socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-
-          const images = []
-
-          for (const image of data.images) {
-            const url = await uploadToCloudinary(image);
-            images.push(url);
-
-          }
-          const chat = new Chat({
-            user_id: userId,
-            content: data.content,
-            images: images
-          });
-    
-          await chat.save();
-    
-          // Trả data ra giao diện realtime
-          _io.emit("SERVER_SEND_MESSAGE", {
-            userId: userId,
-            fullName: fullName,
-            content: data.content,
-            images: images
-          })
-        });
-
-        //  Typing
-        // socket.on("CLIENT_SEND_TYPING", (type) => {
-        //     console.log(type);
-        //     socket.broadcast.emit("SERVER_RETURN_TYPING", {
-        //         userId: userId,
-        //         fullName: fullName,
-        //         type: type
-        //     });
-        // });
-    });
+    chatSocket(res);
 
       // End SocketIO
     
