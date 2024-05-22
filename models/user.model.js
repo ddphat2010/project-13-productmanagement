9f7c660c41df0e80616301cda3e604f4ee370584
chaotic <mnrealest161@gmail.com>
2024-05-22 16:50:55 +0700

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -12,6 +12,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "active"
     },
+    acceptFriends: Array,
+    requestFriends: Array,
+    friendsList: [
+      {
+        user_id: String,
+        room_chat_id: String
+      }
+    ],
     deleted: {
       type: Boolean,
       default: false,
