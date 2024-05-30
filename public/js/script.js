402f2d0be64fd0c6b96b0e0cd67c9c3c83dcd314
chaotic <mnrealest161@gmail.com>
2024-05-30 07:36:59 +0700

    edit css login

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,24 @@
+// formSearch
+// const formSearch = document.querySelector("[formSearch]");
+
+// if(formSearch) {
+//   const url = new URL(window.location.href);
+
+//   formSearch.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const keyword = e.target.elements.keyword.value
+
+//     if(keyword) {
+//       url.searchParams.set("keyword", keyword);
+//     } else {
+//       url.searchParams.delete("keyword")
+//   }
+
+//   window.location.href = url.href;
+//   })
+// }
+// End formSearch
+
 // Show Alert
 const showAlert = document.querySelector("[show-alert]");
 if(showAlert) {
